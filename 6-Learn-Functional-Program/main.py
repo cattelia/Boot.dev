603cf6993fa72bfe3a5f113a2cdeb01486adfe84
cattelia <cattelia@gmail.com>
2024-07-23 12:46:54 -0700

    Push more

--- 6-Learn-Functional-Program/main.py
+++ 6-Learn-Functional-Program/main.py
@@ -1,12 +1,60 @@
-def sum_nested_list(lst):
-    dir_size = 0
-    for file in lst:
-        if type(file) == int:
-            dir_size += file
-            #print("I am a number")
-        if type(file) == list:
-            #print("I am a list")
-            dir_size += sum_nested_list(file)
+def count_nested_levels(nested_documents, target_document_id, level=1):
+    print(f"Currently evaluating {nested_documents}")
+    found = False
+    
+    for doc in nested_documents:
+        if doc == target_document_id:
+            found = True
+            print(f"Level: {level}")
+        
+        if found == False and nested_documents[doc] != {}:
+
+            something = count_nested_levels(nested_documents[doc], target_document_id, level+1)
 
 
-    return dir_size
+    if found == False:
+        return -1
+    return level
+
+
+def count_nested_levels2(nested_documents, target_document_id, level=1):
+
+    if target_document_id in nested_documents:
+        print("Found")
+        return level
+    
+    if nested_documents != {}:
+        for entry in nested_documents:
+            something = count_nested_levels(nested_documents[entry], target_document_id, level + 1)
+            if something != None:
+                return something
+docx1 = {
+    1: {
+        2: {
+            3: {}, 
+            4: {
+                5: {}
+                }
+            }, 
+        6: {}, 
+        7: {
+            8: {
+                9: {
+                    10: {}
+                    }
+                }
+            }
+        }
+}
+
+thing = {
+    1: {
+        3: {}
+    },
+    2: {}
+}
+
+
+#count_nested_levels(thing, target_document_id=3, level=1)
+level = count_nested_levels(docx1, target_document_id=6, level=1)
+print(f"Result {level}")
