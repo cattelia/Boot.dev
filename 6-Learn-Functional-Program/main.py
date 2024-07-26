ea00c6ed1edd2539f2be8f3763fc5b430347f06a
cattelia <cattelia@gmail.com>
2024-07-26 06:49:43 -0700

    Continuing to Function Transformations

--- 6-Learn-Functional-Program/main.py
+++ 6-Learn-Functional-Program/main.py
@@ -1,2 +1,21 @@
-def find_longest_word(content, longest=""):
-    pass
+def doc_format_checker_and_converter(conversion_function, valid_formats):
+
+    def doc_checker(filename, content):
+        check_filename = filename.split(".")
+        if check_filename[1] in valid_formats:
+            return conversion_function(content)
+        else:
+            raise ValueError("Invalid file format")
+    
+    return doc_checker
+        
+
+
+# Don't edit below this line
+
+def capitalize_content(content):
+    return content.upper()
+
+
+def reverse_content(content):
+    return content[::-1]
