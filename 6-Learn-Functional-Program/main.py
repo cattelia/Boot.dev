2bfab5292a3d5487d561318d3e2b0d565b7ad265
cattelia <cattelia@gmail.com>
2024-08-17 11:13:04 -0700

    Currying practice

--- 6-Learn-Functional-Program/main.py
+++ 6-Learn-Functional-Program/main.py
@@ -1,22 +1,7 @@
-def new_clipboard(initial_clipboard):
-    # ?
+def lines_with_sequence(char):
+    def with_char(length):
+        sequence = char * length
 
-    copy_clippy = initial_clipboard.copy()
+        # ?
 
-    def copy_to_clipboard(key, value):
-        copy_clippy[key] = value
-
-    def paste_from_clipboard(key):
-        if key not in copy_clippy:
-            return ""
-        else:
-            return copy_clippy[key]
-
-    return copy_to_clipboard, paste_from_clipboard
-
-'''intitial_clipboard = {'a': 1, 'b': 2, 'c': 3}
-copy_to_clipboard, paste_from_clipboard = new_clipboard(intitial_clipboard)
-copy_to_clipboard('d', 4)
-print(paste_from_clipboard('e'))
-print(paste_from_clipboard('d'))
-'''
+    return with_char
--- 6-Learn-Functional-Program/main_test.py
+++ 6-Learn-Functional-Program/main_test.py
@@ -2,72 +2,66 @@ from main import *
 
 
 run_cases = [
-    [
-        ("Boots", "The Lover of Salmon"),
-        ("Superman", "The Big Blue Boyscout"),
-        ("Batman", "The Caped Crusader"),
-        ("Woman Wonder", ""),
-    ],
+    (
+        "#",
+        3,
+        """###
+@##
+$$$
+###""",
+        2,
+    ),
+    (
+        "$",
+        2,
+        """$$$
+$
+***
+@@@
+$$
+$$$""",
+        3,
+    ),
 ]
-
 
 submit_cases = run_cases + [
-    [
-        ("Green Lantern", "The Man Without Fear"),
-        ("AquaMan", "Dweller in the Depths"),
-        ("The Flash", "The Crimson Comet"),
-        ("The Martian Manhunter", "Mars' Sole Survivor"),
-        ("Cyborg", "Tech Titan"),
-    ],
+    ("%", 1, "", 0),
+    (
+        "*",
+        3,
+        """***
+*
+$$$$$$
+xxx
+****
+***
+***""",
+        4,
+    ),
 ]
 
 
-def test(input_list):
+def test(char, length, doc, expected_output):
     print("---------------------------------")
-    copy_to_clipboard, paste_from_clipboard = new_clipboard({})
-    failed = False
-    for item in input_list:
-        print("Copying to Clipboard:")
-        print(f"*   Key: {item[0]}")
-        print(f"* Value: {item[1]}")
-        copy_to_clipboard(*item)
-
-        print("Pasting From Clipboard:")
-        print(f"*      Key: {item[0]}")
-        result = paste_from_clipboard(item[0])
-        expected_output = item[1]
-        print(f"* Expected: {expected_output}")
-        print(f"*   Actual: {result}")
-        if result != expected_output:
-            print("Fail")
-            failed = True
-        else:
-            print("Pass")
-        print("---------------------------------")
-
-    # check pasting missing key
-    missing_key = "Joker"
-    print("Pasting:")
-    print(f"* Key: {missing_key}")
-    result = paste_from_clipboard(missing_key)
-    expected_output = ""
-    print(f"* Expected: '{expected_output}'")
-    print(f"*   Actual: '{result}'")
-    if result != expected_output:
-        print("Fail: missing key should return an empty string")
-        failed = True
-    else:
+    print(f"Input char: {char}")
+    print(f"Input length: {length}")
+    print(f"Input doc:")
+    print(doc)
+    print(f"Expecting: {expected_output}")
+    num_lines = lines_with_sequence(char)(length)(doc)
+    print(f"Actual: {num_lines}")
+    if num_lines == expected_output:
         print("Pass")
-
-    passed = not failed
-    return passed
+        return True
+    print("Fail")
+    return False
 
 
 def main():
     passed = 0
     failed = 0
     for test_case in test_cases:
-        correct = test(test_case)
+        correct = test(*test_case)
         if correct:
             passed += 1
         else:
