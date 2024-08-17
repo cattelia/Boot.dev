a75fa2ecd419e781c42d95ee601c923b718dbe35
cattelia <cattelia@gmail.com>
2024-08-17 16:07:01 -0700

    Adding incomplete work from Ch 1

--- 6-Learn-Functional-Program/main.py
+++ 6-Learn-Functional-Program/main.py
@@ -1,7 +1,49 @@
-def lines_with_sequence(char):
+'''def lines_with_sequence(char):
     def with_char(length):
         sequence = char * length
 
         # ?
 
-    return with_char
+    return with_char'''
+
+def make_purchases(purchase_orders):
+    leftovers = []
+
+    for item in purchase_orders:
+        try:
+            remaining = purchase(item["price"], item["money_available"])
+            leftovers.append(remaining)
+        except Exception as e:
+            print(e)
+
+    return leftovers
+    
+
+
+def main():
+    print("Making purchases...")
+    leftovers = make_purchases(
+        [
+            {"price": 10.00, "money_available": 125.00},
+            {"price": 5.00, "money_available": 2.00},
+            {"price": 20.01, "money_available": 5.20},
+            {"price": 1.04, "money_available": 254.00},
+            {"price": 40.20, "money_available": 6.00},
+            {"price": 16.00, "money_available": 235.01},
+            {"price": 10.70, "money_available": 10.70},
+            {"price": 12.00, "money_available": 2.30},
+        ]
+    )
+    print("Purchases complete!")
+    print("Leftover amounts for successful purchases:")
+    for leftover in leftovers:
+        print(f" * {leftover:.2f}")
+
+
+def purchase(price, money_available):
+    if money_available < price:
+        raise Exception(f"{money_available:.2f} is not enough for {price:.2f}")
+    return money_available - price
+
+
+main()
