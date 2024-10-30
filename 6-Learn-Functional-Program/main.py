626c14812bbd54bf45ede049ce1d6092c19a0657
Sara <cattelia@gmail.com>
2024-10-30 11:51:07 -0700

    Finished Decorator Challenges

--- 6-Learn-Functional-Program/main.py
+++ 6-Learn-Functional-Program/main.py
@@ -1,5 +1,11 @@
 from functools import lru_cache
 
-# ?
+@lru_cache()
 def is_palindrome(word):
-    pass
+    if len(word) == 1 or len(word) == 0:
+        return True
+        
+    if word[-1] == word[0]:
+        return is_palindrome(word[1:-1])
+    else:
+        return False
