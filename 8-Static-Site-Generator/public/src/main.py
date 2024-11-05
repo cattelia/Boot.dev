71096a6409468be7a871239cd21ba73cc664ac12
Sara <cattelia@gmail.com>
2024-11-05 12:32:01 -0800

    Complete HTMLNode

--- 8-Static-Site-Generator/public/src/main.py
+++ 8-Static-Site-Generator/public/src/main.py
@@ -5,13 +5,8 @@ from textnode import *
 
 
 def main():
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
 
-    t = TextNode("This is a text node", "BOLD", "https://www.boot.dev")
-    #print(t.text, t.text_type, t.url)
-    print(t) # Testing: __repr__()
-
-    s = TextNode("welcome", "italic")
-    #print(s.text, s.text_type, s.url)
-    print(s) # Testing: __repr__()
 
 main()
--- 8-Static-Site-Generator/public/src/textnode.py
+++ 8-Static-Site-Generator/public/src/textnode.py
@@ -1,27 +1,27 @@
 from enum import Enum
 
+
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
-    LINKS = "links"
-    IMAGES = "images"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type.value
+        self.url = url
 
-    def __init__(self, TEXT, TEXT_TYPE, URL=None):
-        self.text = TEXT
-        self.text_type = TEXT_TYPE.value
-        self.url = URL
-
-    def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
-    
     def __eq__(self, other):
         return (
             self.text_type == other.text_type
             and self.text == other.text
             and self.url == other.url
         )
-        
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
