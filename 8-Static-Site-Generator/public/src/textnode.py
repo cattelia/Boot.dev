df3bedf5c16eabdf33d708e72c58f4cc119f6d05
Sara <cattelia@gmail.com>
2024-11-05 01:16:54 -0800

    next step

--- 8-Static-Site-Generator/public/src/textnode.py
+++ 8-Static-Site-Generator/public/src/textnode.py
@@ -12,25 +12,16 @@ class TextNode:
 
     def __init__(self, TEXT, TEXT_TYPE, URL=None):
         self.text = TEXT
-        
-        # TEXT_TYPE is key to TextType.NAME.VALUE but NAME is a func in TextType class
-        #   Need to treat like dictionary. Class[Key].value
-        self.text_type = TextType[TEXT_TYPE.upper()].value
-
-        if URL == None:
-            self.url = URL
-        elif URL != None and URL[0:8] == "https://" or URL[0:7] == "http://":
-            self.url = URL
-        else:
-            self.url = None
+        self.text_type = TEXT_TYPE.value
+        self.url = URL
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
-    def __eq__(self, obj):
-        
-        if self.text == obj.text and self.text_type == obj.text_type and self.url == obj.url:
-            return True
-        else:
-            return False
+    def __eq__(self, other):
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
         
